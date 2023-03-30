f5ea9a9411f81fdad3d2abc78dcc2a9a20c9ab94
bojantomic <tomicb@fon.rs>
2023-03-30 11:54:31 +0200

    Dodati resursi i BibliotekaGUI klasa, i runnable Jar konfiguracija

---
+++ src/main/java/rs/ac/bg/fon/ai/biblioteka_mvn/gui/BibliotekaGUI.java
@@ -0,0 +1,44 @@
+package rs.ac.bg.fon.ai.biblioteka_mvn.gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+
+public class BibliotekaGUI extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					BibliotekaGUI frame = new BibliotekaGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public BibliotekaGUI() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(BibliotekaGUI.class.getResource("/icons/PROJEKTOR-03.png")));
+		setTitle("Biblioteka 2023");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
